f28bf819bd6fa938445a957b676ead0a4c11a9b3
David Wheeler <dwheeler20874@gmail.com>
2026-07-13 18:17:10 -0400

    Switched from 'stick' to 'gilrs' and getting joystick events reliably now.

--- src/main.rs
+++ src/main.rs
@@ -1,142 +1,49 @@
-use std::{task::Poll::{self, Pending, Ready}, time::Duration};
-use stick::{Controller, Event, Listener};
-use tokio::{join, select, sync::oneshot, task::{self, JoinSet, LocalSet}, time::sleep};
-use tokio::sync::oneshot::{Receiver, Sender};
+use std::time::Duration;
+use gilrs::{Gilrs, Button};
+use tokio::{select, time};
 
-type Exit = usize;
 
-struct State {
-    listener: Listener,
-    controllers: Vec<Controller>,
-    rumble: (f32, f32),
-}
 
-impl State {
-    fn connect(&mut self, controller: Controller) -> Poll<Exit> {
-        println!(
-            "Connected p{}, id: {:016X}, name: {}",
-            self.controllers.len() + 1,
-            controller.id(),
-            controller.name(),
-        );
-        self.controllers.push(controller);
-        Pending
+async fn read_js() {
+    let mut interval = time::interval(Duration::from_millis(10));
+    let mut gilrs = Gilrs::new().unwrap();
+
+    // poll Gilrs
+    // Iterate over all connected gamepads
+    for (_id, gamepad) in gilrs.gamepads() {
+        println!("{} is {:?}", gamepad.name(), gamepad.power_info());
     }
 
-    fn event(&mut self, id: usize, event: Event) -> Poll<Exit> {
-        let player = id + 1;
-        println!("p{}: {}", player, event);
-        match event {
-            Event::Disconnect => {
-                self.controllers.swap_remove(id);
-            }
-            Event::MenuR(true) => return Ready(player),
-            Event::ActionA(pressed) => {
-                self.controllers[id].rumble(f32::from(u8::from(pressed)));
-            }
-            Event::ActionB(pressed) => {
-                self.controllers[id].rumble(0.5 * f32::from(u8::from(pressed)));
-            }
-            Event::BumperL(pressed) => {
-                self.rumble.0 = f32::from(u8::from(pressed));
-                self.controllers[id].rumble(self.rumble);
-            }
-            Event::BumperR(pressed) => {
-                self.rumble.1 = f32::from(u8::from(pressed));
-                self.controllers[id].rumble(self.rumble);
-            }
-            _ => {}
+    let mut active_gamepad = None;
+
+    loop {
+        interval.tick().await;
+
+        // Examine new events
+        while let Some(e) = gilrs.next_event() {
+            println!("{:?} New event from {}: {:?}", e.time, e.id, e.event);
+                active_gamepad = Some(e.id);
         }
-        Pending
+
+        // You can also use cached gamepad state
+        if let Some(gamepad) = active_gamepad.map(|id| gilrs.gamepad(id)) {
+            if gamepad.is_pressed(Button::South) {
+                println!("Button South is pressed (XBox - A, PS - X)");
+            }
+        }
+
+        //println!("Tick");
     }
+
 }
 
-async fn stick_task() {
-    let mut js_listener = Listener::default();
-
-    
-    /*
-    // Spawn a task to listen for joystick devices
-    let listen_task = task::spawn(async move {
-        // Listen for joysticks to be added to the system.
-        let listener = Listener::default();
-        loop {
-            let controller = Arc::new(Mutex::new(listener.await));
-            js_tasks.spawn(controller);
-        }
-    });
-    */
-
-    println!("Started stick task.");
-    let mut js_tasks = LocalSet::new();
-
-    join! ( 
-        async {
-            let controller = js_listener.await;
-            js_tasks.spawn_local(controller);
-            println!("Got controller");
+#[tokio::main()]
+async fn main() {
+    select! {
+        _ = read_js() => { 
+            println!("read_js ended.");
         },
-        async {  // TODO-DW : Fix this.
-            let event = js_tasks.await;
-            println!("Event! {event:?}");
-        }
-    );
-
-    /*
-    let mut state = State {
-        listener: Listener::default(),
-        controllers: Vec::new(),
-        rumble: (0.0, 0.0),
     };
 
-    // TODO: Do these things in parallel:
-    //   listen for controllers, process with State::connect method.
-    //   poll controllers, process with State::event method
-    let mut futures = JoinSet::new();
-    let handle = futures.spawn(state.listener);           // Listen for new controllers
-    let listener_id = handle.id();
-    while let Some(Ok((id, result))) = futures.join_next_with_id().await {
-        match id {
-            listener_id => {
-                // Stick listener found a new joystick
-                println!("Found a joystick");
-                state.controllers.push(controller);
-                futures.spawn(controller);
-            }
-            _ => {
-                // joystick event detected
-                println!("Got joystick event");
-            }
-        }
-    }
-    */
-
-    /*
-    let player_id = Loop::new(&mut state)
-        .when(|s| &mut s.listener, State::connect)
-        .poll(|s| &mut s.controllers, State::event)
-        .await;
-    */
-
-    println!("ended the session");
-}
-
-// TODO: Signal end of the program when user presses 'q'
-// (for now, just wait 30 seconds)
-// This task is in a 
-async fn quit_on_q() {
-    sleep(Duration::from_secs(10)).await;
-}
-
-#[tokio::main]
-async fn main() {
-    let local = task::LocalSet::new();
-
-    let t1 = local.run_until(stick_task());
-    let t2 = local.run_until(quit_on_q());
-    select! {
-        _ = t1 => { println!(" Joystick task finished."); },
-        _ = t2 => { println!(" 10 second timeout ended it."); },
-    }
     println!("All done.");
 }

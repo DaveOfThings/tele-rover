693fb1f9473e54eec8f43b8d46b2a06e0f400223
David Wheeler <dwheeler20874@gmail.com>
2026-07-13 22:03:40 -0400

    Added reading of YAML file for MQTT host info and credentials.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::error::Error;
 use gilrs::{Gilrs, Button};
 use tokio::{select, time, task};
 use rumqttc::{MqttOptions, AsyncClient, QoS};
+use serde::Deserialize;
+use std::fs;
 
 
 // Task to open and read game controller
@@ -39,10 +41,26 @@ async fn read_js() {
 
 }
 
+// Define a struct that mirrors your YAML structure
+#[derive(Debug, Deserialize)]
+struct MqttHost {
+    host: String,
+    port: u16,
+    user: String,
+    password: String,
+}
+
 // Task to manage MQTT connection
 async fn manage_mqtt() {
+    let filename = "mqtt-server.yml";
+    let contents = fs::read_to_string(filename)
+        .expect("Could not read mqtt-host.yml file");
+    let host_info: MqttHost = serde_yaml::from_str(&contents)
+        .expect("Could not parse YAML");
+
     // TODO-DW : Convert from rmqttc example to what tele-rover needs
-    let mut mqttoptions = MqttOptions::new("rumqtt-async", "mqtt.local", 1883);
+    let mut mqttoptions = MqttOptions::new("rumqtt-async", host_info.host, host_info.port);
+    mqttoptions.set_credentials(host_info.user, host_info.password);
     mqttoptions.set_keep_alive(Duration::from_secs(5));
 
     let (mut client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
@@ -51,6 +69,7 @@ async fn manage_mqtt() {
     task::spawn(async move {
         for i in 0..10 {
             client.publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize]).await.unwrap();
+            println!("Published {i}");
             time::sleep(Duration::from_millis(100)).await;
         }
     });
